0556eb2d9642e2f23d703186124f008bc570daf9
Phatiski Niry <naphatism@hotmail.com>
2026-03-12 13:39:46 +0700

    Update help.ts

--- help.ts
+++ help.ts
@@ -39,9 +39,12 @@ namespace Polymesh {
     export function fsin(x: number): number {
         // 1. Range Reduction & Folding
         // ปรับ x ให้อยู่ในช่วง [-PI, PI]
-        //x = x % TWO_PI;
-        while (x >  PI) x -= TWO_PI;
-        while (x < -PI) x += TWO_PI;
+        x += PI;
+        x = x % TWO_PI;
+        if (x < 0) x += TWO_PI, x = x % TWO_PI;
+        x -= PI;
+        if (x >  PI) x -= TWO_PI;
+        if (x < -PI) x += TWO_PI;
 
         // 2. Optimized Polynomial
         // สูตร: y = 1.2732 * x - 0.4053 * x * |x|
